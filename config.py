de8ea2a8286afbac5710b0a5d9944cd6db00a1a7
avenue54 <shashutin.tx@gmail.com>
2026-08-02 06:07:53 +0300

    fix

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 from dotenv import load_dotenv
 
+print("CONFIG PATH:", os.getcwd())
+
 load_dotenv()
+
+print("ENV KEY:", os.getenv("VELORA_API_KEY"))
 
 BOT_TOKEN = os.getenv("VELORA_BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
@@ -9,6 +13,25 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 CHANNEL_USERNAME = "@Velora_news"
 CHANNEL_LINK = "https://t.me/Velora_news"
 
-# VELORA API (WireGuard)
 API_URL = os.getenv("VELORA_API_URL", "https://getvelora.xyz").rstrip("/")
 API_KEY = os.getenv("VELORA_API_KEY", "")
+
+
+
+
+
+
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# BOT_TOKEN = os.getenv("VELORA_BOT_TOKEN")
+# ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+# CHANNEL_USERNAME = "@Velora_news"
+# CHANNEL_LINK = "https://t.me/Velora_news"
+
+# # VELORA API (WireGuard)
+# API_URL = os.getenv("VELORA_API_URL", "https://getvelora.xyz").rstrip("/")
+# API_KEY = os.getenv("VELORA_API_KEY", "")
